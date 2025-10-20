75aecf30ecc39d2ce90a875e3ed97e48d914403a
Kiel Stock <mrbasa@gmail.com>
2025-10-19 21:12:40 -0500

    Fixing up Fisher plugin handling.

--- run_onchange_90_update-plugins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Runs 'fisher update' every time 'chezmoi apply' is executed for ongoing maintenance.
-set -eu
-echo "Running: " $(basename $0)
-
-if command -v fish &> /dev/null && fish -c "functions -q fisher"; then
-  echo "Updating Fisher plugins..."
-  fish -c "fisher update"
-fi
