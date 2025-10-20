747af91f2e8c14d420fc4642979038e1bc36ea75
Kiel Stock <mrbasa@gmail.com>
2025-10-20 10:22:52 -0500

    Add .chezscripts/010_install-fish-plugins.sh Add .chezscripts/020_configure-tide.sh Change attributes of .chezscripts/010_install-fisher.sh

---
+++ .chezscripts/run_onchange_after_020_configure-tide.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Applies a one-time, automated configuration to the Tide prompt.
+set -eu
+echo "Running: " $(basename $0)
+
+if fish -c "functions -q tide"; then
+  echo "Applying automatic Tide configuration..."
+  fish -c "tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Dark --show_time='24-hour format' --classic_prompt_separators=Angled --powerline_prompt_heads=Slanted --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Many icons' --transient=Yes"
+else
+  echo "WARNING: 'tide' command not found, skipping configuration." >&2
+fi
